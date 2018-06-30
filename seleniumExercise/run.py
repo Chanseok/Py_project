b47b63dc6f3ce716faa92e905f81dec4a2f514cb
Chanseok <hi007chans@gmail.com>
2018-06-30 22:05:54 +0900

    Change my interest to 파이썬으로 데이터 주무르기

--- seleniumExercise/run.py
+++ seleniumExercise/run.py
@@ -1,3 +1,5 @@
+# * https://www.youtube.com/watch?v=n0PUuYVB90o&index=8&list=PL9mhQYIlKEhf0DKhE-E59fR-iu7Vfpife
+
 # 인터파크 투어 사이트에서 여행지 입력 후 검색 --> 잠시 후 --> 결과
 # 로그인 시 PC 웹 사이트 처리가 어려우면 --> 모바일 로그인 진입
 # 모듈 가져오기 - pip install selenium
@@ -15,5 +17,5 @@ driver.get(main_url)
 
 # 검색창 찾아서 검색어 입력
 # 검색버튼 클릭
-# 잠시 대기 => 페이지 로드 
-# 
+# 잠시 대기 => 페이지 로드
+#
