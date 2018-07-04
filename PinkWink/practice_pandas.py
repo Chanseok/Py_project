658af35993e7b493c20abafb244d5e140869b57d
Chanseok <hi007chans@gmail.com>
2018-07-05 00:27:21 +0900

    List up how to use pandas

--- PinkWink/practice_pandas.py
+++ PinkWink/practice_pandas.py
@@ -9,7 +9,7 @@ dates = pd.date_range('20180703', periods=6)
 dates
 
 #%%
-df = pd.DataFrame(np.random.randn(6,4), 
+df = pd.DataFrame(np.random.randn(6,4), \
                 index = dates, columns=['A', 'B', 'C', 'D'])
 df
 df.head()
@@ -20,3 +20,17 @@ df.values
 
 df.info()
 df.describe()
+df.sort_values(by='B', ascending=False)
+
+df['A']
+df[['A','B']]
+df[0:3]
+df['20180703':'20180705']
+
+df.loc[dates[0]]
+df.loc['20180703']
+df.loc[:,['A','B']]
+df.loc['20180703':'20180705',['A','B']]
+df.loc[dates[0], 'A']
+
+## Github 반영 준비부터 해야 하지 않을까?
